015dbe386f7cbb36836b4187d30e8c4275837fbd
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-11-20 15:41:28 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -30,7 +30,7 @@ def array_literal_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  matrix_lookup(assembled_matrix, 1,1)
+  matrix_lookup[(assembled_matrix, 1,1)]
   # Return the matrix's content at row and column
 end
 
